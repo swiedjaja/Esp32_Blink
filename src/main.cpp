cd29001645f065f9e8181f82fdc8ad77d3ed0e07
swiedjaja <swiedjaja@gmail.com>
2022-01-11 11:09:00 +0700

    ver-1.1 merubah ke define

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#if defined(ESP8266)
+  #define ON 0
+#elif defined(ESP32) 
+  #define ON 1
+#endif  
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,17 +14,12 @@ void setup() {
 int nCount =0;
 void loop() {
   Serial.printf("Hello %d\n", nCount++);
-#if defined(ESP8266)
-  int nActive = LOW;
-#elif defined(ESP32) 
-  int nActive = HIGH;
-#endif  
   // put your main code here, to run repeatedly:
-    digitalWrite(LED_BUILTIN, nActive);
+    digitalWrite(LED_BUILTIN, ON);
   // wait for a second
   delay(20);
   // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, !nActive);
+  digitalWrite(LED_BUILTIN, 1-ON);
    // wait for a second
   delay(1000);
 }
